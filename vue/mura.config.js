2cde56464279887d76b4d03171fa2db9fbf51520
Matt Levine <matt.levine@blueriver.com>
2019-01-08 17:44:35 -0800

    Adding example of client based collection rendering

--- vue/mura.config.js
+++ vue/mura.config.js
@@ -1,5 +1,6 @@
 import Mura from "mura.js";
 import Example from '@/components/modules/Example'
+import CollectionLayout from '@/components/modules/CollectionLayout'
 import Vue from 'vue'
 
 require('mura.js/src/core/ui.vue')
@@ -15,7 +16,13 @@ Mura.loader()
 	.loadcss(Mura.corepath + '/modules/v1/core_assets/css/mura.7.1.min.css')
 	.loadcss(Mura.corepath + '/modules/v1/core_assets/css/mura.7.1.skin.css');
 
-Mura.Module.example=Mura.UI.Vue.extend(
+Mura.Module.Example=Mura.UI.Vue.extend(
  {
 	component:Example
+});
+
+//This is registered as a collection layout in the mura.config.json
+Mura.Module.VueCollectionLayout=Mura.UI.Vue.extend(
+ {
+	component:CollectionLayout
 });
---
+++ vue/src/components/modules/CollectionLayout.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+	<ul>
+		<li v-for="item in context.collection.getAll().items">
+			<a v-bind:href="item.url">{{item.menutitle}}</a>
+		</li>
+	</ul>
+	<button v-if="context.collection.has('first')"  v-on:click="goToPage('first')">First</button>
+	<button v-if="context.collection.has('previous')"  v-on:click="goToPage('previous')">&lt;&lt; Previous</button>
+	<button v-if="context.collection.has('next')"  v-on:click="goToPage('next')">Next &gt;&gt;</button>
+	<button v-if="context.collection.has('last')"  v-on:click="goToPage('last')">Last</button>
+	</div>
+</template>
+
+<script>
+
+import Mura from 'mura.js'
+
+export default {
+	name: 'collection-layout',
+	props: ['context'],
+	methods:{
+		goToPage:function(link){
+			link=link || 'missing';
+			if(this.context.collection.has(link)){
+				this.context.collection.get(link).then((collection)=>{
+					this.context.collection=collection;
+				})
+			}
+		}
+	}
+}
+</script>
